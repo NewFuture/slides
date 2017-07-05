05e0414fd178b4a7bf5b737e9d9fc6aef0119c6c
New Future <github@newfuture.cc>
2017-07-05 22:58:35 +0800

    update svg

--- _plugins/remote_include.rb
+++ _plugins/remote_include.rb
@@ -1,18 +1,17 @@
 require 'net/http'
 require 'uri'
 
-module Jekyll
+def open(url)
+  puts url
+  Net::HTTP.get(URI.parse(url.strip)).force_encoding 'utf-8'
+end
 
+module Jekyll
   class RemoteInclude < Liquid::Tag
 
     def initialize(tag_name, markup, tokens)
       super
       @markup = markup
-    end
-
-    def open(url)
-      puts url
-      Net::HTTP.get(URI.parse(url.strip)).force_encoding 'utf-8'
     end
 
     def render(context)
@@ -24,4 +23,15 @@ module Jekyll
   end
 end
 
+module Jekyll
+  module DownloadFilter
+
+    def download(text)
+      open(text)
+    end
+    
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::DownloadFilter)
 Liquid::Template.register_tag('remote_include', Jekyll::RemoteInclude)
